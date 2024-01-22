ca74e8bab7b71dc574087668ff42a9603ae947d6
RogCode <cenriquegomezc@gmail.com>
2024-01-22 03:22:08 +0000

    first commit

--- ch2/exercises/Exercise2_8.java
+++ ch2/exercises/Exercise2_8.java
@@ -6,7 +6,7 @@ public class Exercise2_8 {
         char speciesFirstLetter = 'T'; //First letter of T-Rex
 
         //Print the variable
-        System.out.println("First letter of finosaur species: " + speciesFirstLetter);
+        System.out.println("First letter of dinosaur species: " + speciesFirstLetter);
 
     }
 }
